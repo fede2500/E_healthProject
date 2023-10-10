aea43e7c8991611fb7fee6e647dce2cead31a52c
fede2500 <federico.tognoni.25@gmail.com>
2023-10-10 16:09:46 +0200

    add calocolo varianza questionari

--- Project.py
+++ Project.py
@@ -22,6 +22,40 @@ df_heas = df.iloc[:, 29:42]
 df_ccs = df.iloc[:, 42:54]
 #print(df_ccs.head(3))
 
+fig, axs = plt.subplots(df_phq.shape[1], figsize=(8, 12))
+
+# Assegna un colore diverso a ciascun dato
+colors = plt.cm.viridis(np.linspace(0, 1, df_phq.shape[0]))
+
+for i in range(df_phq.shape[1]):
+    axs[i].scatter(range(df_phq.shape[0]), df_phq.iloc[:, i], c=colors)
+    axs[i].set_xlabel('Dati')
+    axs[i].set_ylabel(f'Feature {i+1}')
+    axs[i].set_title(f'Feature {i+1} vs Dati')
+
+plt.tight_layout()
+plt.show()
+
+def calcolo_var(df, n):
+    varianza = []
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            df.iloc[i,j] = round(df.iloc[i,j]/n, 2)
+        v = round(np.nanvar(df.iloc[i,:]), 4)
+        varianza.append(v)
+    print(len(varianza))
+    return round(np.mean(varianza),4)
+
+print("la varianza di phq è: ", calcolo_var(df_phq,df_phq.max().max()))
+
+print("la varianza di gad è: ", calcolo_var(df_gad,df_gad.max().max()))
+
+print("la varianza di eheals è: ", calcolo_var(df_eheals,df_eheals.max().max()))
+
+
+
+
+
 
 #substitute the NaN values with the mean of the answers to that questionnaires
 #can be changed with the median or mode
@@ -46,7 +80,7 @@ def fill_nan_cols(dataf):
         for j in range(dataf.shape[1]):
             if pd.isnull(dataf.iloc[i, j]):
                 M = round(moda(dataf.iloc[:,j]))
-                print(M)
+                #print(M)
                 dataf.iloc[i, j] = M
     return dataf
 
@@ -65,7 +99,7 @@ df_inf = fill_nan_cols(df_inf)
 df_complete = pd.concat([df_inf, df_phq, df_gad, df_eheals, df_heas, df_ccs], axis=1, ignore_index=True)
 #print(df_complete)
 #print(df_complete.info())
-print(df_complete.isnull().sum())
+#print(df_complete.isnull().sum())
 
 
 
