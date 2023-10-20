89075081a80b4a26b00544778e294b073c0f12f6
eleonora11sala <eleonora1.sala@mail.polimi.it>
2023-10-20 16:39:37 +0200

    add fdma

---
+++ personasCRI/executeFDMA.py
@@ -0,0 +1,29 @@
+import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+def runPCA(data):
+    # Find the best PCA components
+    nums = np.arange(25)
+    var_ratio = []
+    for num in nums:
+        pca = PCA(n_components=num)
+        pca.fit(data)
+        var_ratio.append(np.sum(pca.explained_variance_ratio_))
+
+    plt.figure()
+    plt.grid()
+    plt.plot(nums, var_ratio, marker='o')
+    plt.xlabel('n_components')
+    plt.ylabel('Explained variance ratio')
+    plt.title('n_components vs. Explained Variance Ratio')
+
+    plt.show()
+
+    # definitive pca
+    pca_sel = PCA(n_components=15)
+    scores = pca_sel.fit_transform(data)
+    print(pca_sel.explained_variance_ratio_)
+    print(f'Total variance explained: {pca_sel.explained_variance_ratio_.sum()}%')
+
+    return scores, pca_sel
