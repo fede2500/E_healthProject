023d8466f43f0580fbe5acab1b155e1114e380e5
eleonora11sala <eleonora1.sala@mail.polimi.it>
2023-10-20 13:46:30 +0200

    add pyplot to prepareData

--- personasCRI/dataPreparation.py
+++ personasCRI/dataPreparation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.impute import SimpleImputer
 
