062c6ffca4dd9a92082988bfed75bb9f0a75307c
ChriDiDi <christian.didiego@mail.polimi.it>
2023-10-24 15:35:38 +0200

    display mwu p values in paare

--- personasCRI/mwu.py
+++ personasCRI/mwu.py
@@ -13,6 +13,8 @@ def mwu(originalClusterized):
     # result_df = pd.DataFrame(columns=['Variabile', 'P-Value Kruskal', "Result Kruskal", "P-value MWY", "Result MWY"])
     result_df = pd.DataFrame(columns=['Variabile', "Result MWU"])
 
+    summary_p_values = pd.DataFrame(columns=['Variabile', "p 0 vs 1", "Diff 0 1", "p 0 vs 2", "Diff 0 2", "p 1 vs 2", "Diff 1 2",])
+
     alpha = 0.05 / 3  # Livello di significatività , /3 per Bonferroni (?)
 
     for variabile in variabili_da_testare:
@@ -42,14 +44,34 @@ def mwu(originalClusterized):
         # tra tutte le coppie, 1 altrimenti
 
         isDifferenceSign = 1
+        diff_0_1 = "Diff. significative"
+        diff_0_2 = "Diff. significative"
+        diff_1_2 = "Diff. significative"
+        result_0_1 = 0
+        result_0_2 = 0
+        result_1_2 = 0
 
         for chiave, (statistiche, p_value) in risultati.items():
-            # i, j = chiave
-            # print(f"Mann-Whitney tra Gruppo {i} e Gruppo {j}:")
-            # print(f"Statistiche U: {statistiche}")
-            # print(f"Valore p: {p_value}")
-            if p_value >= alpha:
+             i, j = chiave
+             print(f"Mann-Whitney tra Cluster {i} e Cluster {j}:")
+             if(i == 0 and j == 1):
+                 result_0_1 = p_value
+                 if p_value >= alpha:
+                     diff_0_1 = "Diff. NON significative"
+             if(i == 0 and j == 2):
+                 result_0_2 = p_value
+                 if p_value >= alpha:
+                     diff_0_2 = "Diff. NON significative"
+             if(i == 1 and j == 2):
+                 result_1_2 = p_value
+                 if p_value >= alpha:
+                     diff_1_2 = "Diff. NON significative"
+
+             print(f"Valore p: {p_value}")
+             if p_value >= alpha:
                 isDifferenceSign = 0
+
+
 
         # se isDifferenceSign non è mai andato a 0, vuol dire che la diff è significativa
         # per ogni coppia
@@ -59,5 +81,8 @@ def mwu(originalClusterized):
             string_result_mwu = "Diff. NON significative"
         result_df.loc[len(result_df)] = [variabile, string_result_mwu]
 
+        summary_p_values.loc[len(summary_p_values)] = [variabile, result_0_1, diff_0_1, result_0_2, diff_0_2, result_1_2, diff_1_2]
 
-    return result_df
+
+
+    return result_df, summary_p_values
